2c1088b8f495c7f3dd731f88db7885ad1fca0686
Jack Henza <jackhenza@gmail.com>
2023-12-05 10:49:58 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- server/app.py
+++ server/app.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my website!</h1>'
+
+if __name__ == "__main__":
+    app.run(port = 7777, debug = True)
